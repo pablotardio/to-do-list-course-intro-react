0ec908c4d41054e57934078ff45c4e76afb830f4
pablotardio <pablo.tv30@gmail.com>
2022-10-23 21:40:49 -0400

    feature: added TodoError component

--- src/components/App/AppUI.js
+++ src/components/App/AppUI.js
@@ -10,6 +10,7 @@ import { TodoContext } from "../../context/TodoContext";
 import { Modal } from "../Modal";
 import { TodoForm } from "../TodoForm";
 import { TodoItemLoading } from "../loading/TodoItemLoading";
+import { TodoError } from "../TodoError/TodoError";
 const defaultTodos = [
 	{ text: "Cortar cebolla", completed: false },
 	{ text: "Tomar el curso de intro a React", completed: false },
@@ -26,9 +27,9 @@ function AppUI() {
 			<TodoSearch />
 			<TodoList>
 				{loading && new Array(2).fill(1).map((item=><TodoItemLoading/>))}
-				{error && <p>Hubo un error </p>}
+				{error && <TodoError/>}
 				{!loading && !todos.length && <p>crea tu primerTodo </p>}
-				{todos.map((todo) => (
+				{!error&&todos.map((todo) => (
 					<TodoItem
 						{...todo}
 						onComplete={handlers.completeTodo}
